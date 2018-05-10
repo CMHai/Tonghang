9646c4dfc0e5824feebdfabc1f0e432d26b19fcb
CMHai <33917925+CMHai@users.noreply.github.com>
2018-05-10 18:09:26 +0800

    Add files via upload

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,18 +2,18 @@
   <div class="">
     <div class="chunk1">
       <img src="../assets/banner.png" style="width:100%; z-index: -1;" alt="">
-      <div class="container">
-        <div id="navbar" class="hidden-xs hidden-sm">
+      <div id="navbar" class="hidden-xs hidden-sm">
+        <div class="container">
           <div class="row">
             <div class="col-md-7">
-              <div class="col-md-10 col-md-offset-2">
-                <img src="../assets/logo.png" style="width: 17.6rem;">
+              <div class="">
+                <img src="../assets/logo.png" style="width: 13.2rem;">
               </div>
             </div>
             <div class="col-md-5" style="font-size:1.56rem;">
               <div class="row">
                 <div class="col-md-3">
-                  <span @click="scrollTo()">首页</span>
+                  <span class="active" @click="scrollTo()">首页</span>
                 </div>
                 <div class="col-md-3">
                   <span @click="scrollTo('.chunk3')">系统特点</span>
@@ -34,12 +34,16 @@
       <div class="container">
         <div class="row">
           <div class="col-md-8">
-            <div class="text1">作为通用航空领域的软件系统方案提供商，我们拥有</div>
-            <div class="text2">丰富的通航业务经验、完整的软件开发团队、以及经验丰富的工程师</div>
+            <div class="text1">
+              作为通用航空领域的软件系统方案提供商，<br>我们拥有
+              <span style="color: #fedc91;">丰富的通航业务经验、完整的软件开发团队、</span>
+              以及
+              <span style="color: #fedc91;">经验丰富的工程师</span>
+            </div>
           </div>
           <div class="col-md-4">
             <div class="botton">
-              <img src="../assets/phone.png" alt="" style="margin:-1.2rem 1.5rem 0 2.76rem;">
+              <img src="../assets/phone.png" alt="" style="margin:-1.2rem 1.5rem 0 3rem; width: 2rem">
               <span class="num">010-62662080</span>
             </div>
           </div>
@@ -52,79 +56,77 @@
           <div class="title">系统特点</div>
           <div style="text-align:center;margin-bottom:6rem;"><img src="../assets/line.png" alt=""></div>
         </div>
-        <div class="row">
-          <div class="col-md-12">
+        <div class="row" id="xttd">
+
+          <div class="col-md-4" style="padding-bottom:2rem;">
             <div class="row">
-              <div class="col-md-4" style="padding-bottom:2rem;">
-                <div class="row">
-                  <div class="col-md-2" style="text-align: center;">
-                    <img src="../assets/icon1.png" alt="">
-                  </div>
-                  <div class="col-md-10">
-                    <div class="lable">实用性</div>
-                    <div class="synopsis">系统上线2年，实现了无纸化办公 <br>满足业务各方面要求，得到用户好评</div>
-                  </div>
-                </div>
+              <div class="col-md-2 xttdimg">
+                <img src="../assets/icon1.png" alt="">
               </div>
-              <div class="col-md-4" style="padding-bottom:2rem;">
-                <div class="row">
-                  <div class="col-md-2" style="text-align: center;">
-                    <img src="../assets/icon2.png" alt="">
-                  </div>
-                  <div class="col-md-10">
-                    <div class="lable">安全性</div>
-                    <div class="synopsis">内外网隔离，保证系统的安全性<br> 数据热备份，保证数据的安全性</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4" style="padding-bottom:2rem;">
-                <div class="row">
-                  <div class="col-md-2" style="text-align: center;">
-                    <img src="../assets/icon3.png" alt="">
-                  </div>
-                  <div class="col-md-10">
-                    <div class="lable">稳定性</div>
-                    <div class="synopsis">系统长时间运行，经受了时间和 <br> 数据的考验，系统运行平稳</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4" style="padding-bottom:2rem;">
-                <div class="row">
-                  <div class="col-md-2" style="text-align: center;">
-                    <img src="../assets/icon4.png" alt="">
-                  </div>
-                  <div class="col-md-10">
-                    <div class="lable">准确性</div>
-                    <div class="synopsis">系统自动校验，保证数据一致性 <br> 系统自动计算，杜绝人为出错可能性 <br> 非法数据无法录入到系统里</div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4" style="padding-bottom:2rem;">
-                <div class="row">
-                  <div class="col-md-2" style="text-align: center;">
-                    <img src="../assets/icon5.png" alt="">
-                  </div>
-                  <div class="col-md-10">
-                    <div class="lable">效率提升</div>
-                    <div class="synopsis">电子化的录入、自动化计算<br> 自动提示基础数据，减轻记忆负担 <br> 自动生成报表，为决策提供支持<br> 通过数据分析找到系统优化点，提高效率</div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4" style="padding-bottom:2rem;">
-                <div class="row">
-                  <div class="col-md-2" style="text-align: center;">
-                    <img src="../assets/icon6.png" alt="">
-                  </div>
-                  <div class="col-md-10">
-                    <div class="lable">实时性</div>
-                    <div class="synopsis">实时提供现场数据，<br>为指挥大厅决策提供依据</div>
-                  </div>
-                </div>
+              <div class="col-md-10">
+                <div class="lable">实用性</div>
+                <div class="synopsis">系统上线2年，实现了无纸化办公 <br>满足业务各方面要求，得到用户好评</div>
               </div>
             </div>
           </div>
+          <div class="col-md-4" style="padding-bottom:2rem;">
+            <div class="row">
+              <div class="col-md-2 xttdimg">
+                <img src="../assets/icon2.png" alt="">
+              </div>
+              <div class="col-md-10">
+                <div class="lable">安全性</div>
+                <div class="synopsis">内外网隔离，保证系统的安全性<br> 数据热备份，保证数据的安全性</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4" style="padding-bottom:2rem;">
+            <div class="row">
+              <div class="col-md-2 xttdimg">
+                <img src="../assets/icon3.png" alt="">
+              </div>
+              <div class="col-md-10">
+                <div class="lable">稳定性</div>
+                <div class="synopsis">系统长时间运行，经受了时间和 <br> 数据的考验，系统运行平稳</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4" style="padding-bottom:2rem;">
+            <div class="row">
+              <div class="col-md-2 xttdimg">
+                <img src="../assets/icon4.png" alt="">
+              </div>
+              <div class="col-md-10">
+                <div class="lable">准确性</div>
+                <div class="synopsis">系统自动校验，保证数据一致性 <br> 系统自动计算，杜绝人为出错可能性 <br> 非法数据无法录入到系统里</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4" style="padding-bottom:2rem;">
+            <div class="row">
+              <div class="col-md-2 xttdimg">
+                <img src="../assets/icon5.png" alt="">
+              </div>
+              <div class="col-md-10">
+                <div class="lable">效率提升</div>
+                <div class="synopsis">电子化的录入、自动化计算<br> 自动提示基础数据，减轻记忆负担 <br> 自动生成报表，为决策提供支持<br> 通过数据分析找到系统优化点，提高效率</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4" style="padding-bottom:2rem;">
+            <div class="row">
+              <div class="col-md-2 xttdimg">
+                <img src="../assets/icon6.png" alt="">
+              </div>
+              <div class="col-md-10">
+                <div class="lable">实时性</div>
+                <div class="synopsis">实时提供现场数据，<br>为指挥大厅决策提供依据</div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
@@ -152,12 +154,15 @@
               <li data-target="#carousel-example-generic" data-slide-to="4">
                 大屏显示
               </li>
+              <div style="font-size: 1.8rem;width: 21.6rem; margin: auto;line-height: 4.2rem;padding-left: 4rem;">
+                ......
+              </div>
             </ul>
           </div>
           <div class="col-md-7">
 
             <div class="mac">
-              <div id="carousel-example-generic" class="carousel slide" data-interval="5000" data-ride="carousel">
+              <div id="carousel-example-generic" class="carousel slide" data-interval="3000" data-ride="carousel">
 
                 <!-- Wrapper for slides -->
                 <div class="carousel-inner" role="listbox">
@@ -192,7 +197,7 @@
         <div class="row">
           <div class="col-md-5">
             <div class="introduce">
-              <div class="lable">北京新锐友信软件技术开发公司</div>
+              <div class="lable">北京新锐友信软件技术有限公司</div>
               <div><img src="../assets/point.png"> 提供通用航空领域软件系统解决方案</div>
               <div><img src="../assets/point.png"> 丰富的通航业务经验</div>
               <div><img src="../assets/point.png"> 完整的软件开发团队</div>
@@ -226,22 +231,45 @@ export default {
   },
   mounted() {
     $(window).scroll(function() {
-      if ($(this).scrollTop() > 200) {
+      const chunk3Top = $(".chunk3").offset().top - 300;
+      const chunk4Top = $(".chunk4").offset().top - 300;
+      const chunk5Top = $(".chunk5").offset().top - 300;
+      const scrollTop = $(this).scrollTop();
+      if (scrollTop > 180) {
         $("#navbar").css({
           position: "fixed",
           top: "0",
           background: "#fff",
-          color: "#43B1F8",
           "box-shadow": "0 2px 4px rgba(0, 0, 0, 0.14)"
         });
       } else {
         $("#navbar").css({
           position: "absolute",
-          top: "7rem",
+          top: "4.7rem",
           background: "none",
-          color: "#878787",
           "box-shadow": "0 0px 0px rgba(0, 0, 0, 0.14)"
         });
+      }
+      if (scrollTop < chunk3Top) {
+        $("#navbar span.active").removeClass("active");
+        $("#navbar span")
+          .eq(0)
+          .addClass("active");
+      } else if (scrollTop < chunk4Top) {
+        $("#navbar span.active").removeClass("active");
+        $("#navbar span")
+          .eq(1)
+          .addClass("active");
+      } else if (scrollTop < chunk5Top) {
+        $("#navbar span.active").removeClass("active");
+        $("#navbar span")
+          .eq(2)
+          .addClass("active");
+      } else if (scrollTop > chunk5Top) {
+        $("#navbar span.active").removeClass("active");
+        $("#navbar span")
+          .eq(3)
+          .addClass("active");
       }
     });
     $("#carousel-example-generic").on("slide.bs.carousel", function(event) {
@@ -293,9 +321,10 @@ html {
   position: relative;
 }
 #navbar {
-  color: #878787;
+  color: #999999;
+  font-weight: bold;
   position: absolute;
-  top: 7rem;
+  top: 4.7rem;
   left: 0;
   line-height: 6rem;
   width: 100%;
@@ -303,9 +332,12 @@ html {
   /* background: #fff; */
   /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.14);  */
 }
+#navbar span.active {
+  color: #43b1f8;
+}
 #navbar span:hover {
   cursor: pointer;
-  color: #38a0f6;
+  color: #43b1f8;
 }
 .chunk2 {
   position: relative;
@@ -313,9 +345,9 @@ html {
   background-color: #4c4c4c;
 }
 .text1 {
-  margin-top: 1rem;
+  margin: 1rem 0;
   font-family: PingFang-SC-Bold;
-  font-size: 1.7rem;
+  font-size: 1.6rem;
   font-weight: normal;
   font-stretch: normal;
   line-height: 3.3rem;
@@ -323,15 +355,8 @@ html {
   color: #ffffff;
 }
 
-.text2 {
-  margin-bottom: 1rem;
-  font-family: PingFang-SC-Bold;
-  font-size: 1.7rem;
-  line-height: 3.3rem;
-  color: #fedc91;
-}
 .botton {
-  margin: 1rem;
+  margin: 1rem 0;
   min-height: 4.6rem;
   border-radius: 3rem;
   border: solid 0.06rem #ffffff;
@@ -361,7 +386,15 @@ html {
   text-align: center;
   margin-bottom: 1.32rem;
 }
-
+#xttd {
+  margin-left: 3%;
+}
+.xttdimg {
+  text-align: center;
+}
+.xttdimg img {
+  width: 2.8rem;
+}
 .chunk3 .lable {
   min-height: 2.28rem;
   font-family: PingFang-SC-Bold;
@@ -410,8 +443,8 @@ html {
 }
 
 #hp-indicators li {
-  padding-right: 3rem;
-  text-align: center;
+  padding-left: 4rem;
+  /* text-align: center; */
   list-style-type: none;
   width: 21.6rem;
   margin: auto;
@@ -441,7 +474,7 @@ html {
   padding: 6rem 0;
   min-height: 43.2rem;
   font-family: PingFang-SC-Regular;
-  font-size: 1.44rem;
+  font-size: 1.2rem;
 }
 .line {
   width: 50%;
@@ -453,6 +486,7 @@ html {
   margin-bottom: 1.8rem;
   /* height: 2.52rem; */
   font-size: 1.8rem;
+  font-weight: bold;
   color: #000000;
 }
 .chunk5 .tel {
@@ -470,16 +504,18 @@ html {
 }
 .introduce img {
   margin-right: 1rem;
+  width: 2.2rem;
 }
 .footer {
   padding: 2.34rem;
   background-color: #1f7dbc;
   font-family: PingFang-SC-Regular;
-  font-size: 1.44rem;
+  font-size: 1.2rem;
   color: #ffffff;
   text-align: center;
   line-height: 2;
 }
+
 @media (max-width: 768px) {
   .chunk3 .lable {
     margin-top: 1rem;
@@ -488,6 +524,9 @@ html {
   .chunk3 .synopsis {
     font-size: 1.4rem;
     text-align: center;
+  }
+  #xttd {
+    margin-left: 0;
   }
 }
 @media (max-width: 500px) {
